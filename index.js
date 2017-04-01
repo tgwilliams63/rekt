10e8a78fb05ff1664857be3c11b1271ece2ef1f1
Travis Williams <travis.williams63@gmail.com>
2017-04-01 18:11:16 -0400

    Attempt to Add Icon
    Adding Icon...

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 'use strict';
-const request = require('request')
+const co = require('co');
+const request = require('co-request')
 const querystring = require('querystring')
+const token = require('./token.json').token
 // import Alpr from 'alpr';
 // import rollDice from 'rollDice';
 
@@ -8,32 +10,25 @@ console.log('Loading function');
 
 let rektList = ["Not rekt","Rekt","Really Rekt","Tyrannosaurus Rekt","Cash4Rekt.com","Grapes of Rekt","Ship Rekt","Rekt markes the spot","Caught rekt handed","The Rekt Side Story","Singin' In The Rekt","Painting The Roses Rekt","Rekt Van Winkle","Parks and Rekt","Lord of the Rekts: The Reking of the King","Star Trekt","The Rekt Prince of Bel-Air","A Game of Rekt","Rektflix","Rekt it like it's hot","RektBox 360","The Rekt-men","School Of Rekt","I am Fire, I am Rekt","Rekt and Roll","Professor Rekt","Catcher in the Rekt","Rekt-22","Harry Potter: The Half-Rekt Prince","Great Rektspectations","Paper Scissors Rekt","RektCraft","Grand Rekt Auto V","Call of Rekt: Modern Reking 2","Legend Of Zelda: Ocarina of Rekt","Rekt It Ralph","Left 4 Rekt","www.rekkit.com","Pokemon: Fire Rekt","The Shawshank Rektemption","The Rektfather","The Rekt Knight","Fiddler on the Rekt","The Rekt Files","The Good, the Bad, and The Rekt","Forrekt Gump","The Silence of the Rekts","The Green Rekt","Gladirekt","Spirekted Away","Terminator 2: Rektment Day","The Rekt Knight Rises","The Rekt King","REKT-E","Citizen Rekt","Requiem for a Rekt","REKT TO REKT ass to ass","Star Wars: Episode VI - Return of the Rekt","Braverekt","Batrekt Begins","2001: A Rekt Odyssey","The Wolf of Rekt Street","Rekt's Labyrinth","12 Years a Rekt","Gravirekt","Finding Rekt","The Arekters","There Will Be Rekt","Christopher Rektellston","Hachi: A Rekt Tale","The Rekt Ultimatum","Shrekt","Rektal Exam","Rektium for a Dream","www.Trekt.tv","Erektile Dysfunction","2Girls1Rek"]
 
-exports.handler = function(event, context) {
+function* main(event,context){
     console.log('Rekt')
 
     let options = null
 
     let webhookURL = 'https://hooks.slack.com/services/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'
     let response = null
-    let responseURL = querystring.parse(event.body).response_url
-    let userName = querystring.parse(event.body).user_name
-    let userID = querystring.parse(event.body).user_id
-    let channelID = querystring.parse(event.body).channel_id
+    let parsedBody = querystring.parse(event.body)
+    let responseURL = parsedBody.response_url
+    let userName = parsedBody.user_name
+    let userID = parsedBody.user_id
+    let channelID = parsedBody.channel_id
+    let userInfoURL = 'https://slack.com/api/users.info?token=' + token +'&user=' + userID
 
-    option = {
-        uri : 'https://slack.com/api/users.profile.get',
-        method : 'POST',
-        json : {
-            "token" : "xoxp-7112906229-7112618374-133616337748-a2ed9cd721d851170cde930280790526",
-            "user" : userID
-        }
-
-    }
-
-    request('https://slack.com/api/users.profile.get',)
+    let userInfo = yield request(userInfoURL)
+    let imageURL = JSON.parse(userInfo).user.profile.image_original
     
     console.log("Event Body: " + event.body)
-    console.log("Raw URL: " + querystring.parse(event.body).response_url)
+    console.log("Raw URL: " + parsedBody.response_url)
     console.log("Response URL: " + responseURL)
 
     let max = rektList.length - 1
@@ -52,6 +47,11 @@ exports.handler = function(event, context) {
         "channel" : channelID, 
         "response_type" : "in_channel",
         "text" : ">" + text
+        "attachments" : [
+            {
+                "author_icon" : imageURL
+            }
+        ]
       }
     };
 
@@ -71,5 +71,8 @@ exports.handler = function(event, context) {
       }
       context.succeed(response);
     });
+}
 
+exports.handler = function(event, context) {
+    co(main(event,context))
 };
